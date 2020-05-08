4491c0faf411ad04530ba56ad608197b8b02e399
kwakwoohyun010@gmail.com <kwakwoohyun010@gmail.com>
2020-05-08 12:07:29 +0900

    feat: Add routes and orm

--- learn-sequelize/app.js
+++ learn-sequelize/app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// Route
+var commentsRouter = require('./routes/comments')
+
 // DB 추가 부분
 var sequelize = require('./models').sequelize
 
@@ -19,13 +22,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/comments', commentsRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- learn-sequelize/routes/index.js
+++ learn-sequelize/routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
+
+var User = require('../models').User
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+/*
+    시퀄라이즈는 프로미스를 기본적으로 지원하므로 then과 catch를 사용해서 각각
+    조회 성공 시와 실패 시의 정보를 얻을 수 있다. 
+*/
+router.get('/', (req, res, next) => {
+  // User.findAll 메서드로 모든 사용자를 찾은 후, sequelize.pug를 렌더링할 때 결괏값인 users를 넣어준다. 
+  User.findAll()
+    .then((users) => {
+      res.render('sequelize', { users })
+    })
+    .catch((err) => {
+      console.error(err)
+      next(err)
+    })
+})
+/*
+async/await 문법으로 표현
 
+router.get('/', async (req, res, next) => {
+  try {
+    const users = await User.findAll()
+    res.render('sequelize', { users })
+  } catch (err) {
+    console.error(err)
+    next(err)
+  }
+})
+*/
 module.exports = router;
